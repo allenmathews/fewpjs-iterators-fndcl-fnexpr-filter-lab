352285cf6f922d6f0e5e1cc98cf9f49c39d7e434
Allen Mathews <allenjoemathews@gmail.com>
2021-11-23 07:48:53 +1100

    done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, str) {
+    return drivers.filter((driver) => {
+        return driver.toLowerCase() === str.toLowerCase();
+    });
+}
+
+function fuzzyMatch(drivers, str) {
+    return drivers.filter((driver) => {
+        return str.toLowerCase() === driver.substring(0, str.length).toLowerCase();
+    })
+}
+
+function matchName(drivers, str) {
+    return drivers.filter((driver) => {
+        return driver.name === str
+    })
+}
