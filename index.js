eb557a2534dcf878449cd8725b590a82bb1db2b6
Allen Mathews <allenjoemathews@gmail.com>
2023-02-20 18:29:56 +1100

    commit tree b7c61f7526c9808742b460be39db6104fa223ba5

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
 // Code your solution here
-function findMatching(drivers, str) {
-    return drivers.filter((driver) => {
-        return driver.toLowerCase() === str.toLowerCase();
+// function findMatching(drivers, str) {
+//     return drivers.filter((driver) => {
+//         return driver.toLowerCase() === str.toLowerCase();
+//     });
+// }
+
+// function fuzzyMatch(drivers, str) {
+//     return drivers.filter((driver) => {
+//         return str.toLowerCase() === driver.substring(0, str.length).toLowerCase();
+//     })
+// }
+
+// function matchName(drivers, str) {
+//     return drivers.filter((driver) => {
+//         return driver.name === str
+//     })
+// }
+
+
+function findMatching(drivers, name) {
+    return drivers.filter(driver => {
+        if (driver.toLowerCase() === name.toLowerCase()) {
+            return driver;
+        }
     });
 }
 
-function fuzzyMatch(drivers, str) {
-    return drivers.filter((driver) => {
-        return str.toLowerCase() === driver.substring(0, str.length).toLowerCase();
-    })
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
 }
 
-function matchName(drivers, str) {
-    return drivers.filter((driver) => {
-        return driver.name === str
+function matchName(drivers, name) {
+    return drivers.filter(driver => {
+        if (driver.name === name) {
+            return driver.name;
+        }
     })
 }
